d28de6cf70de58fec1e0873b74114994348fc8e7
David Torres <dabiddo@gmail.com>
2025-03-22 16:09:41 +0000

    upgraded to lates astro version

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '絆メキシコ';
+export const SITE_DESCRIPTION = 'メキシコ人がメキシコを紹介する';
+export const SITE_KEYWORDS = 'メキシコ、スペイン語、';
